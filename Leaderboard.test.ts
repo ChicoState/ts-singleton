23c92bbb866a3556d3a12a3ac19f54f5c0ccf17f
Kevin Buffardi <kbuffardi@csuchico.edu>
2023-03-28 22:05:17 -0700

    Add singleton tests

--- Leaderboard.test.ts
+++ Leaderboard.test.ts
@@ -5,7 +5,14 @@ describe("Leaderboard tests", ()=> {
     let list: Leaderboard = Leaderboard.get_instance();
     expect(list.as_string()).toBe("1. Empty 0\n2. Empty 0\n3. Empty 0\n4. Empty 0\n5. Empty 0\n");
   });
-  test("Leaderboard should be a singleton so second instance should duplicate first if both instances are captured first", () => {
+  test("Leaderboard by default should have 5 Empty Spots, each with 0 score", () => {
+    let list: Leaderboard = Leaderboard.get_instance();
+    expect(list.as_string()).toBe("1. Empty 0\n2. Empty 0\n3. Empty 0\n4. Empty 0\n5. Empty 0\n");
+  });
+});
+
+describe("Leaderboard should be a singleton", ()=> {
+  test("Second instance of Leaderboard should duplicate first if both instances are captured first", () => {
     let initial: Leaderboard = Leaderboard.get_instance();
     let duplicate: Leaderboard = Leaderboard.get_instance();
     initial.add_leader("fifth",1);
@@ -15,7 +22,7 @@ describe("Leaderboard tests", ()=> {
     initial.add_leader("first",5);
     expect(initial.as_string()).toBe(duplicate.as_string());
   });
-  test("Leaderboard should be a singleton so second instance should duplicate first even after initialization", () => {
+  test("Second instance of Leaderboard should duplicate first even after first is initialized", () => {
     let initial: Leaderboard = Leaderboard.get_instance();
     initial.add_leader("fifth",1);
     initial.add_leader("fourth",2);
@@ -25,4 +32,14 @@ describe("Leaderboard tests", ()=> {
     let duplicate: Leaderboard = Leaderboard.get_instance();
     expect(initial.as_string()).toBe(duplicate.as_string());
   });
-})
+  test("Changes to second instance of Leaderboard reflect changes in first", () => {
+    let initial: Leaderboard = Leaderboard.get_instance();
+    let duplicate: Leaderboard = Leaderboard.get_instance();
+    duplicate.add_leader("fifth",1);
+    duplicate.add_leader("fourth",2);
+    duplicate.add_leader("third",3);
+    duplicate.add_leader("second",4);
+    duplicate.add_leader("first",5);
+    expect(initial.as_string()).toBe(duplicate.as_string());
+  });
+});
